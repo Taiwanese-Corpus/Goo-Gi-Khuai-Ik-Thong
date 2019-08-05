9894ca04f6daeffa1b50391fb512a781cf88c993
a8568730 <a8568730@hotmail.com>
2019-08-05 12:55:37 +0800

    * tshì lia̍h Tâi-gí im-tóng

---
+++ liah.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#m f j 
+#target_kiap="m"
+# Huagi: g
+# Taigi: j f  
+# Haffa: c h d r z l"
+# Tsokgi: "a t b n p"
+# English: "e"
+target_kiap="j f"
+
+for kiap in $target_kiap
+do
+  echo $kiap
+  mkdir -p "${kiap}_files"
+
+  for num in $(seq -f "%03g" 1 200)
+  do
+    url="http://163.21.182.5/ez5language/ezlearn/FM/${kiap}_files/${num}.mp3"
+    wget --directory-prefix=./${kiap}_files ${url}
+  done
+
+done
