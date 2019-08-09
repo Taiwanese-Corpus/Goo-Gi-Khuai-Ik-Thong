102d5fc289fd295f037100767636d7f9a6331cd6
a8568730 <a8568730@hotmail.com>
2019-08-09 18:55:46 +0800

    * 用指令共原始xml檔案先tsuán做utf-8，才吐csv

--- liah.bash
+++ liah.bash
@@ -9,7 +9,28 @@
 # English: "e"
 
 
-curl "http://163.21.182.5/ez5language/ezlearn/FM/{g,m,j,f,c,h,d,r,z,l,a,t,b,n,p,e}.xml" --create-dirs -o "./xml/#1.xml"
+#curl "http://163.21.182.5/ez5language/ezlearn/FM/{g,m,j,f,c,h,d,r,z,l,a,t,b,n,p,e}.xml" --create-dirs -o "./xml/#1.xml"
+
+
+# Convert encoding
+mkdir -p utf8_xml
+
+for i in $(file xml/* | grep -v 'UTF-8' | sed 's/:.*//g')
+do 
+ cat $i | iconv -f big5 -t utf-8 > utf8_${i}
+done
+
+for i in $(file xml/* | grep 'UTF-8' | sed 's/:.*//g')
+do
+  cp $i utf8_${i}
+  
+  # Remove garbled character
+  sed -i "s/$(echo -ne '\ude86')//g" utf8_${i}
+done
+
+
+find utf8_xml/ -type f  -exec bash -c 'grep "<node name=" {} | awk -F "[/\"]" "{print \$2, \$4, \$5,     substr(\$9, 4)}" OFS=, >  {}.csv' \;
+
 exit
 
 target_kiap="g m j f c h d r z l a t b n p e"
